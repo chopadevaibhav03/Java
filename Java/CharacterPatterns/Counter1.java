34b22619f0ad959bf7a9433ac4b147ac1ea945af
8975060028 <107178252+8975060028@users.noreply.github.com>
2025-05-18 11:50:16 +0530

    unnecessary files and folder removed

--- Java/CharacterPatterns/Counter1.java
+++
@@ -1,7 +0,0 @@
-class Counter1
-{
-	public static void main(String []args)
-	{
-		System.out.println("Hello");
-	}
-}
--- Java/Test.java
+++
@@ -1,9 +0,0 @@
-class Test
-{
-public static void main(String args[])
-  {
-	int a = 1;
-	System.out.println(++a + a++ - --a);
-	
-  }
-}
